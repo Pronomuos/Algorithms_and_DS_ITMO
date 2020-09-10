9068ffcdec37c46fadada8a251444e50caa58f98
Pronomuos <71034403+Pronomuos@users.noreply.github.com>
2020-09-10 22:45:42 +0300

    Create task_B.cpp

---
+++ lab11/task_B.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <set>
+ 
+using namespace std;
+ 
+void dijkstra (int st, vector<vector<pair<int, int>>> &g, vector<int> &d) {
+    d[st] = 0;
+    set < pair<int,int> > q;
+    q.insert (make_pair (d[st], st));
+    while (!q.empty()) {
+        int v = q.begin()->second;
+        q.erase (q.begin());
+ 
+        for (auto edge : g[v]) {
+            int to = edge.first,
+                    len = edge.second;
+            if (d[v] + len < d[to]) {
+                q.erase (make_pair (d[to], to));
+                d[to] = d[v] + len;
+                q.insert (make_pair (d[to], to));
+            }
+        }
+    }
+}
+ 
+int main() {
+ 
+    ifstream input_file ("pathsg.in");
+    ofstream output_file("pathsg.out");
+ 
+ 
+    int ver, edges;
+    input_file >> ver >> edges;
+    vector<vector<pair<int, int>>> g (ver);
+ 
+    for (int i = 0; i < edges; i++) {
+        int ver1, ver2, cost;
+        input_file >> ver1 >> ver2 >> cost;
+        ver1--; ver2--;
+        g[ver1].push_back(make_pair(ver2, cost));
+    }
+ 
+    vector<int> d(ver);
+    for (int i = 0; i < ver; i++) {
+        d.assign(ver, INT_MAX);
+        dijkstra(i, g, d);
+        for (int j = 0; j < ver; j++)
+            output_file << d[j] << " ";
+        output_file << "\n";
+    }
+ 
+    output_file.close();
+    input_file.close();
+ 
+    return 0;
+}
